15905156492adf363ea178c17d47224d4f9e55f8
Jack Kelly <kcjackkelly@gmail.com>
2023-04-15 22:42:48 -0500

    add back intro section

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,15 +1,16 @@
 import clsx from "clsx";
 import type { NextPage } from "next";
-import MetaMaskCard from "../components/MetaMaskCard";
 import { Navigation } from "../components/Navigation";
 import { Heading } from "../components/primitives/Heading";
+import { Image } from "../components/primitives/Image";
 import { InnerColumn, OuterColumn, PageWrapper, Section } from "../components/primitives/Layout";
 import { tw } from "../utils/tw";
+
 const Column = tw.div`flex-1`;
 
 const ColumnInner = tw.div`mt-4 rounded-md border bg-primitive-faint border-primitive-edge p-3 flex flex-col space-y-1`;
 
-const ColumnWrapper = tw.div`mt-4 flex flex-row gap-4`;
+const ColumnWrapper = tw.div`flex flex-row gap-4`;
 
 const ColumnOfferItem = tw.div`block pb-1 border-b border-primitive-edge-faint last:pb-0 last:border-b-0`;
 
@@ -61,10 +62,40 @@ const Home: NextPage = () => {
 			<Navigation />
 
 			<OuterColumn>
+				<div className="relative w-full overflow-hidden rounded-xl bg-primary-faint">
+					<div className="opacity-60 absolute left-0 bottom-0 top-0">
+						<Image
+							src="/static/bg_curve.png"
+							alt="bg doce"
+							width={3176}
+							priority={false}
+							height={1880}
+							placeholder="empty"
+							className="delay-[2s] duration-[3s]"
+						/>
+					</div>
+
+					<div className="absolute top-1/4 left-[-7%] -translate-y-1/2 transform">
+						<Image
+							src="/static/gradient_two.png"
+							width={1250}
+							height={1250}
+							alt="gradient"
+							placeholder="empty"
+							className="delay-300 duration-1000"
+						/>
+					</div>
+
+					<InnerColumn className="py-10 md:py-20 lg:scroll-py-32">
+						<Heading className="relative !text-white text-center" size="xxl" weight="semibold">
+							The future of <br />
+							user ownership
+						</Heading>
+					</InnerColumn>
+				</div>
+
 				<Section>
 					<InnerColumn width="third">
-						<MetaMaskCard />
-
 						<ColumnWrapper>
 							<Column>
 								<Heading>Buy Tokens</Heading>
